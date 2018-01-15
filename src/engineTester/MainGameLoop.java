ea915aec19df001ab324d9b26881558ab611c3ba
Waseem Koya <waseemkoya@gmail.com>
2018-01-15 23:34:35 +1300

    created entities, view and projection matrices and camera

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,10 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -16,39 +19,126 @@ public class MainGameLoop {
 		
 		DisplayManager.createDisplay();	
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		// OpenGL expects vertices to be defined counter clockwise by default
-		float[] vertices = {
-				-0.5f,0.5f,0,	//V0
-				-0.5f,-0.5f,0,	//V1
-				0.5f,-0.5f,0,	//V2
-				0.5f,0.5f,0		//V3
-		};
+//		// OpenGL expects vertices to be defined counter clockwise by default
+//		float[] vertices = {
+//				-0.5f,0.5f,0,	//V0
+//				-0.5f,-0.5f,0,	//V1
+//				0.5f,-0.5f,0,	//V2
+//				0.5f,0.5f,0		//V3
+//		};
+//		
+//		int[] indices = {
+//				0,1,3,	//Top left triangle (V0,V1,V3)
+//				3,1,2	//Bottom right triangle (V3,V1,V2)
+//		};
+//		
+//		float[] textureCoords = {
+//				0,0,		//V0
+//				0,1,		//V1
+//				1,1,		//V2
+//				1,0			//V3
+//		};
 		
-		int[] indices = {
-				0,1,3,	//Top left triangle (V0,V1,V3)
-				3,1,2	//Bottom right triangle (V3,V1,V2)
+		//CUBE TEST
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
+				
 		};
 		
 		float[] textureCoords = {
-				0,0,		//V0
-				0,1,		//V1
-				1,1,		//V2
-				1,0			//V3
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,			
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0
+
+				
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 		
 		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("pokeball"));
-		TexturedModel texturedModel = new TexturedModel(model,texture);
+		TexturedModel staticModel = new TexturedModel(model,texture);
 		
+		Camera camera = new Camera();
+		
+		Entity entity = new Entity(staticModel,new Vector3f(0,0,-5),0,0,0,1);
 		
 		while(!Display.isCloseRequested()) {
 			//game logic
+			//entity.increasePosition(0, 0, -0.1f);
+			entity.increaseRotation(1,1,0);
+			camera.move();
 			renderer.prepare();
 			shader.start();
-			renderer.render(texturedModel);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity,shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -12,34 +12,32 @@ public class DisplayManager {
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
 	private static final int FPS_CAP = 60;
-	
+
 	public static void createDisplay() {
-		
-		ContextAttribs attribs = new ContextAttribs(3,2);
-		attribs.withForwardCompatible(true);
-		attribs.withProfileCore(true);
-		
+
+		ContextAttribs attribs = new ContextAttribs(3, 2).withForwardCompatible(true).withProfileCore(true);
+
 		try {
-			Display.setDisplayMode(new DisplayMode(WIDTH,HEIGHT));
-			Display.create(new PixelFormat(),attribs);
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create(new PixelFormat(), attribs);
 			Display.setTitle("Java 3D Game");
 		} catch (LWJGLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		GL11.glViewport(0, 0, WIDTH, HEIGHT);
 	}
-	
+
 	public static void updateDisplay() {
-		
+
 		Display.sync(FPS_CAP);
 		Display.update();
-		
+
 	}
-	
+
 	public static void closeDisplay() {
-		
+
 		Display.destroy();
 	}
 }
