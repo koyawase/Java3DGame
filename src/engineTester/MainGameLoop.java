d0f88d64e0b16a3790732cedf8588786bfa9f407
Waseem Koya <waseemkoya@gmail.com>
2018-02-03 19:38:49 +1300

    display now uses .obj files to render objects to display, rather than inputting arrays required values

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -22,118 +23,19 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 		
-//		// OpenGL expects vertices to be defined counter clockwise by default
-//		float[] vertices = {
-//				-0.5f,0.5f,0,	//V0
-//				-0.5f,-0.5f,0,	//V1
-//				0.5f,-0.5f,0,	//V2
-//				0.5f,0.5f,0		//V3
-//		};
-//		
-//		int[] indices = {
-//				0,1,3,	//Top left triangle (V0,V1,V3)
-//				3,1,2	//Bottom right triangle (V3,V1,V2)
-//		};
-//		
-//		float[] textureCoords = {
-//				0,0,		//V0
-//				0,1,		//V1
-//				1,1,		//V2
-//				1,0			//V3
-//		};
 		
-		//CUBE TEST
-		float[] vertices = {			
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,0.5f,-0.5f,		
-				
-				-0.5f,0.5f,0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				0.5f,0.5f,-0.5f,	
-				0.5f,-0.5f,-0.5f,	
-				0.5f,-0.5f,0.5f,	
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,-0.5f,	
-				-0.5f,-0.5f,-0.5f,	
-				-0.5f,-0.5f,0.5f,	
-				-0.5f,0.5f,0.5f,
-				
-				-0.5f,0.5f,0.5f,
-				-0.5f,0.5f,-0.5f,
-				0.5f,0.5f,-0.5f,
-				0.5f,0.5f,0.5f,
-				
-				-0.5f,-0.5f,0.5f,
-				-0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,-0.5f,
-				0.5f,-0.5f,0.5f
-				
-		};
-		
-		float[] textureCoords = {
-				
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,			
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-				
-		};
-		
-		int[] indices = {
-				0,1,3,	
-				3,1,2,	
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,	
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-		
-		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("pokeball"));
+		RawModel model = OBJLoader.loadObjModel("stall", loader);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("stallTexture"));
 		TexturedModel staticModel = new TexturedModel(model,texture);
 		
 		Camera camera = new Camera();
 		
-		Entity entity = new Entity(staticModel,new Vector3f(0,0,-5),0,0,0,1);
+		Entity entity = new Entity(staticModel,new Vector3f(0,0,-50),0,0,0,1);
 		
 		while(!Display.isCloseRequested()) {
 			//game logic
 			//entity.increasePosition(0, 0, -0.1f);
-			entity.increaseRotation(1,1,0);
+			entity.increaseRotation(0,1,0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
