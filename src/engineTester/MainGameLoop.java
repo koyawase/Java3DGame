37e0a4dd0b098e5a83c18056e3f3ec54207fd6c5
Waseem Koya <waseemkoya@gmail.com>
2018-02-07 22:31:41 +1300

    implemented specular lighting to work alongside diffused lighting

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -26,12 +26,15 @@ public class MainGameLoop {
 		
 		
 		RawModel model = OBJLoader.loadObjModel("dragon", loader);
-		ModelTexture texture = new ModelTexture(loader.loadTexture("white"));
-		TexturedModel staticModel = new TexturedModel(model,texture);
+		
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("white")));
+		ModelTexture texture = staticModel.getTexture();
+		texture.setShineDamper(10);
+		texture.setReflectivity(1);
 		
 		Camera camera = new Camera();
 		
-		Entity entity = new Entity(staticModel,new Vector3f(0,-3,-25),0,0,0,1);
+		Entity entity = new Entity(staticModel,new Vector3f(0,-5,-30),0,0,0,1);
 		Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
 		
 		while(!Display.isCloseRequested()) {
