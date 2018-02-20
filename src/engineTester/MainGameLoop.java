11cabd85a883ecba0267c870aa9da80dc4773725
Waseem Koya <waseemkoya@gmail.com>
2018-02-20 20:56:47 +1300

    updated project to be the same as that of the tutorial. https://www.youtube.com/watch?v=yNYwZMmgTJk&index=14&list=PLRIWtICgwaX0u7Rf9zkZhLoLuZVfUksDP.

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,52 +1,67 @@
 package engineTester;
-
-import org.lwjgl.opengl.Display;
-import org.lwjgl.util.vector.Vector3f;
-
-import entities.Camera;
-import entities.Entity;
-import entities.Light;
+ 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+ 
 import models.RawModel;
 import models.TexturedModel;
+ 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+ 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import renderEngine.OBJLoader;
+import terrains.Terrain;
 import textures.ModelTexture;
-
+import entities.Camera;
+import entities.Entity;
+import entities.Light;
+ 
 public class MainGameLoop {
-
-	public static void main(String args[]) {
-		
-		DisplayManager.createDisplay();	
-		Loader loader = new Loader();
-
-		
-		RawModel model = OBJLoader.loadObjModel("dragon", loader);
-		
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
-		ModelTexture texture = staticModel.getTexture();
-		texture.setShineDamper(10);
-		texture.setReflectivity(1);
-		
-		Camera camera = new Camera();
-		
-		Entity entity = new Entity(staticModel,new Vector3f(0,-5,-30),0,0,0,1);
-		Light light = new Light(new Vector3f(0,0,-20), new Vector3f(1,1,1));
-		
-		MasterRenderer renderer = new MasterRenderer();
-		while(!Display.isCloseRequested()) {
-			//game logic
-			//entity.increasePosition(0, 0, -0.1f);
-			entity.increaseRotation(0,1,0);
-			camera.move();
-			renderer.processEntity(entity);
-			renderer.render(light, camera);
-			DisplayManager.updateDisplay();
-		}
-		
-		renderer.cleanUp();
-		loader.cleanUp();
-		DisplayManager.closeDisplay();
-	}
+ 
+    public static void main(String[] args) {
+ 
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+         
+         
+        RawModel model = OBJLoader.loadObjModel("tree", loader);
+         
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
+         
+        List<Entity> trees = new ArrayList<Entity>();
+        Random random = new Random();
+        for(int i=0;i<500;i++){
+            trees.add(new Entity(staticModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
+        }
+         
+        Light light = new Light(new Vector3f(0,0,-20),new Vector3f(1,1,1));
+         
+        Terrain terrain = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2 = new Terrain(1,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+         
+        Camera camera = new Camera();   
+        MasterRenderer renderer = new MasterRenderer();
+         
+        while(!Display.isCloseRequested()){
+            camera.move();
+             
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+            for(Entity entity:trees){
+                renderer.processEntity(entity);
+            }
+            renderer.render(light, camera);
+            DisplayManager.updateDisplay();
+        }
+ 
+        renderer.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+ 
+    }
+ 
 }
