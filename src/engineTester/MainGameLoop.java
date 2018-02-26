b0a1dd0ec1e5c64447c3859f5e3b3e415e7165b9
Waseem Koya <waseemkoya@gmail.com>
2018-02-26 21:04:18 +1300

    added multitexturing to add multiple textures onto a terrain using a blend map

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -17,6 +17,8 @@ import renderEngine.Loader;
 import renderEngine.MasterRenderer;
 import terrains.Terrain;
 import textures.ModelTexture;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
@@ -28,10 +30,26 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
         Loader loader = new Loader();
          
-
+        //********** TERRAIN TEXTURE STUFF **********
+        
+        TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grass"));
+        TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("mud"));
+        TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("grassFlowers"));
+        TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
+        
+        TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture, rTexture, gTexture, bTexture);
+        TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
+        
+        //*******************************************
+        
+        
         ModelData treeData = OBJFileLoader.loadOBJ("tree");
         RawModel treeModel = loader.loadToVAO(treeData.getVertices(), treeData.getTextureCoords(), treeData.getNormals(), treeData.getIndices());
-        TexturedModel staticTree = new TexturedModel(treeModel,new ModelTexture(loader.loadTexture("lowPolyTree")));
+        TexturedModel staticTree = new TexturedModel(treeModel,new ModelTexture(loader.loadTexture("tree")));
+        
+        ModelData lowTreeData = OBJFileLoader.loadOBJ("lowPolyTree");
+        RawModel lowTreeModel = loader.loadToVAO(lowTreeData.getVertices(), lowTreeData.getTextureCoords(), lowTreeData.getNormals(), lowTreeData.getIndices());
+        TexturedModel lowstaticTree = new TexturedModel(lowTreeModel,new ModelTexture(loader.loadTexture("lowPolyTree")));
         
         ModelData grassData = OBJFileLoader.loadOBJ("grassModel");
         RawModel grassModel = loader.loadToVAO(grassData.getVertices(), grassData.getTextureCoords(), grassData.getNormals(), grassData.getIndices());
@@ -48,18 +66,20 @@ public class MainGameLoop {
         List<Entity> trees = new ArrayList<Entity>();
         List<Entity> grass = new ArrayList<Entity>();
         List<Entity> ferns = new ArrayList<Entity>();
+        List<Entity> lpTrees = new ArrayList<Entity>();
         Random random = new Random();
         for(int i=0;i<500;i++){
             trees.add(new Entity(staticTree, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
             grass.add(new Entity(staticGrass,new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,1));
-            ferns.add(new Entity(staticFern,new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,1));
+            ferns.add(new Entity(staticFern,new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,0.75f));
+            lpTrees.add(new Entity(lowstaticTree, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,0.40f));
         }
               
          
         Light light = new Light(new Vector3f(100,100,-20),new Vector3f(1,1,1));
          
-        Terrain terrain = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
-        Terrain terrain2 = new Terrain(1,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain = new Terrain(0,-1,loader,texturePack,blendMap);
+        Terrain terrain2 = new Terrain(1,-1,loader,texturePack,blendMap);
          
         Camera camera = new Camera();   
         MasterRenderer renderer = new MasterRenderer();
@@ -77,6 +97,9 @@ public class MainGameLoop {
             }
             for(Entity fern : ferns) {
             	renderer.processEntity(fern);
+            }
+            for(Entity lpTree : lpTrees) {
+            	renderer.processEntity(lpTree);
             }
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
